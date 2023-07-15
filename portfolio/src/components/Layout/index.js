f0970f7746d54b216bf115a1b235d9273407e372
deadsxnpai <truearclight@gmail.com>
2023-07-15 14:31:56 +0300

    fixes

---
+++ portfolio/src/components/Layout/index.js
@@ -0,0 +1,12 @@
+import './index.scss'
+import React, { Component } from 'react';
+
+export default class Layout extends Component {
+    render() {
+        return (
+            <>
+            <h5>Hello</h5>
+            </>
+        );
+    }
+}
